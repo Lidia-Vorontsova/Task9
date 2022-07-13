f95096e686d0de94e7c78238309f98b7d9caf8c6
Lidia <lidiya_vorontsov@mail.ru>
2022-07-13 16:09:52 +0300

    task9

---
+++ task9/Program.cs
@@ -0,0 +1,16 @@
+﻿int number = new Random().Next(10, 100);
+Console.WriteLine($"Наше случайное число = {number}");
+
+int secondDigit = number % 10;
+int firstDigit = number / 10;
+
+Console.WriteLine($"первая цифра = {firstDigit}, вторая цифра = {secondDigit}");
+
+if (firstDigit > secondDigit)
+{
+    Console.WriteLine($"первая цифра числа {number} больше. Это {firstDigit}");
+}
+else
+{
+     Console.WriteLine($"вторая цифра числа {number} больше. Это {secondDigit}");
+}
